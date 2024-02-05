e59632c839f2861caae4f9b08ccee0068f45d70c
Shruti Kapile 2216 <153350871+ShrutiKapile@users.noreply.github.com>
2024-02-05 04:22:29 -0800

    Add files via upload

---
+++ Program544.java
@@ -0,0 +1,10 @@
+import java.awt.*;
+
+//class GUI1
+class Program544 {
+    public static void main(String Arg[]) {
+        Frame fobj = new Frame("Marvellous");
+        fobj.setSize(600, 600);
+        fobj.setVisible(true);
+    }
+}
